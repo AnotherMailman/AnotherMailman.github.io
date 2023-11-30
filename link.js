89f18ce6cbc53825c1966ccc58eb77fe8b56a4c4
Liam Eriksson <lier0419@storsjogymnasiet.se>
2023-11-30 10:40:03 +0100

    Gjorde linksida!

---
+++ link.js
@@ -0,0 +1,18 @@
+function gammalDirect() {
+    window.location.replace("Gammalsida/index.html")
+}
+function gissaDirect() {
+    window.location.replace("Gissa/Gissa.htm")
+}
+function gridDirect() {
+    window.location.replace("Gridtest/grid.html")
+}
+function htmlDirect() {
+    window.location.replace("html_element/index.html")
+}
+function knappenDirect() {
+    window.location.replace("Knappen/Knappen.html")
+}
+function webbDirect() {
+    window.location.replace("webbhistoria/historia.html")
+}
