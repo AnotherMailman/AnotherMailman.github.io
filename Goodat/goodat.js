ead788f41fd6654564688439a104ccc8ffd200ff
Liam Eriksson <lier0419@storsjogymnasiet.se>
2024-01-18 11:47:58 +0100

    18/1-24

---
+++ Goodat/goodat.js
@@ -0,0 +1,10 @@
+function cooKing() {
+}
+function BaKing() {
+
+}
+function LaunDering() {
+document.body.style.backgroundImage = url('https://image.cnbcfm.com/api/v1/image/104632927-GettyImages-467287994.jpg?v=1532563784');
+
+
+}
