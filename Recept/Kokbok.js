5b046d24e2dee3894425741e515037805049e162
Liam Eriksson <lier0419@storsjogymnasiet.se>
2024-02-08 11:29:19 +0100

    8/2-2024

---
+++ Recept/Kokbok.js
@@ -0,0 +1,18 @@
+function spaghettiDirect() {
+    window.location.replace("Bolognese.html")
+}
+function gulashDirect() {
+    window.location.replace("Gulashsoppa.html")
+}
+function Direct() {
+    window.location.replace("")
+}
+function Direct() {
+    window.location.replace("")
+}
+function Direct() {
+    window.location.replace("")
+}
+function Direct() {
+    window.location.replace("")
+}
